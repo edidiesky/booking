a4ddcfe302118cc379a84b852e1a33a3e0f40210
edidiesky <essienedidiong1000@gmail.com>
2024-06-04 04:34:24 -0700

    new fixes

--- app/dashboard/reservation/_components/Calendar.jsx
+++ app/dashboard/reservation/_components/Calendar.jsx
@@ -18,7 +18,7 @@ const ReservationCalendar = () => {
    const [filterButtonState, setFilterButtonState] = useState(0);
 const isLoading = false
    return (
-     <section className='relative h-[600px]'>
+     <section className='relative p4 bg-white overflow-hidden shadow-lg border rounded-[10px] min-h-[450px]'>
        <Scheduler
          data={mockedSchedulerData}
          isLoading={isLoading}
@@ -54,18 +54,18 @@ const mockedSchedulerData = [
     data: [
       {
         id: "8b71a8a5-33dd-4fc8-9caa-b4a584ba3762",
-        startDate: new Date("2023-04-13T15:31:24.272Z"),
-        endDate: new Date("2023-08-28T10:28:22.649Z"),
+        startDate: new Date("2024-04-13T15:31:24.272Z"),
+        endDate: new Date("2024-08-28T10:28:22.649Z"),
         occupancy: 3600,
-        title: "Project A",
+        title: "Room",
         subtitle: "Subtitle A",
         description: "array indexing Salad West Account",
         bgColor: "rgb(254,165,177)",
       },
       {
         id: "22fbe237-6344-4c8e-affb-64a1750f33bd",
-        startDate: new Date("2023-10-07T08:16:31.123Z"),
-        endDate: new Date("2023-11-15T21:55:23.582Z"),
+        startDate: new Date("2024-10-07T08:16:31.123Z"),
+        endDate: new Date("2024-11-15T21:55:23.582Z"),
         occupancy: 2852,
         title: "Project B",
         subtitle: "Subtitle B",
@@ -74,8 +74,8 @@ const mockedSchedulerData = [
       },
       {
         id: "3601c1cd-f4b5-46bc-8564-8c983919e3f5",
-        startDate: new Date("2023-03-30T22:25:14.377Z"),
-        endDate: new Date("2023-09-01T07:20:50.526Z"),
+        startDate: new Date("2024-03-30T22:25:14.377Z"),
+        endDate: new Date("2024-09-01T07:20:50.526Z"),
         occupancy: 1800,
         title: "Project C",
         subtitle: "Subtitle C",
@@ -83,8 +83,57 @@ const mockedSchedulerData = [
       },
       {
         id: "b088e4ac-9911-426f-aef3-843d75e714c2",
-        startDate: new Date("2023-10-28T10:08:22.986Z"),
-        endDate: new Date("2023-10-30T12:30:30.150Z"),
+        startDate: new Date("2024-10-28T10:08:22.986Z"),
+        endDate: new Date("2024-10-30T12:30:30.150Z"),
+        occupancy: 11111,
+        title: "Project D",
+        subtitle: "Subtitle D",
+        description: "Garden heavy an software Metal",
+        bgColor: "rgb(254,165,177)",
+      },
+    ],
+  },
+  {
+    id: "070ac5b5-8369-4cd2-8ba2-0a209130cc60",
+    label: {
+      icon: "https://picsum.photos/24",
+      title: "Alfred Samuel",
+      subtitle: "Frontend Developer",
+    },
+    data: [
+      {
+        id: "8b71a8a5-33dd-4fc8-9caa-b4a584ba3762",
+        startDate: new Date("2024-04-20T15:31:24.272Z"),
+        endDate: new Date("2024-09-28T10:28:22.649Z"),
+        occupancy: 3600,
+        title: "Room A",
+        subtitle: "Subtitle A",
+        description: "array indexing Salad West Account",
+        bgColor: "rgb(254,165,177)",
+      },
+      {
+        id: "22fbe237-6344-4c8e-affb-64a1750f33bd",
+        startDate: new Date("2024-10-21T08:16:31.123Z"),
+        endDate: new Date("2024-11-24T21:55:23.582Z"),
+        occupancy: 2852,
+        title: "Project B",
+        subtitle: "Subtitle B",
+        description: "Tuna Home pascal IP drive",
+        bgColor: "rgb(254,165,177)",
+      },
+      {
+        id: "3601c1cd-f4b5-46bc-8564-8c983919e3f5",
+        startDate: new Date("2024-03-10T22:25:14.377Z"),
+        endDate: new Date("2024-09-11T07:20:50.526Z"),
+        occupancy: 1800,
+        title: "Project C",
+        subtitle: "Subtitle C",
+        bgColor: "rgb(254,165,177)",
+      },
+      {
+        id: "b088e4ac-9911-426f-aef3-843d75e714c2",
+        startDate: new Date("2024-10-18T10:08:22.986Z"),
+        endDate: new Date("2024-10-20T12:30:30.150Z"),
         occupancy: 11111,
         title: "Project D",
         subtitle: "Subtitle D",
--- app/dashboard/reservation/_components/index.jsx
+++ app/dashboard/reservation/_components/index.jsx
@@ -18,7 +18,7 @@ const DashboardIndex = () => {
       </AnimatePresence>
       <div className="w-full relative flex flex-col gap-12">
         <div className="w-full grid lg:grid-cols-2 lg:items-center gap-4 justify-between">
-          <h3 className="text-4xl font-booking_font_normal">
+          <h3 className="text-3xl font-booking_font_normal">
             My Reservation
           </h3>
           <div className="flex items-center lg:justify-end gap-2">
