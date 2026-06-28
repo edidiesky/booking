30f5cb9c9a2a3c771b458155dc00a0a0ea9bf0bc
edidiesky <essienedidiong1000@gmail.com>
2026-06-28 07:22:47 +0100

    feat(routes): add ProtectRoute guard redirecting unauthenticated users to login

---
+++ frontend/src/routes/guards/ProtectRoute.tsx
@@ -0,0 +1,17 @@
+import { useSelector }     from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+import { selectAccessToken, selectCurrentUser } from "@/redux/slices/authSlice";
+
+interface Props { children: React.ReactNode; }
+
+export function ProtectRoute({ children }: Props) {
+  const accessToken = useSelector(selectAccessToken);
+  const currentUser = useSelector(selectCurrentUser);
+  const location    = useLocation();
+
+  if (!accessToken || !currentUser) {
+    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+  }
+
+  return <>{children}</>;
+}
