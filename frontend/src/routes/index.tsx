c92c46971625cbb863c2fcd7aeffe1083c32d5d9
edidiesky <essienedidiong1000@gmail.com>
2026-06-28 07:23:26 +0100

    feat(routes): compose browser router merging all route arrays

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import { authRoutes }          from "./authRoutes";
+import { guestRoutes }         from "./guestRoutes";
+import { dashboardRoutes }     from "./dashboardRoutes";
+import { adminRoutes }         from "./adminRoutes";
+
+export const router = createBrowserRouter([
+  ...guestRoutes,
+  ...authRoutes,
+  ...dashboardRoutes,
+  ...adminRoutes,
+]);
